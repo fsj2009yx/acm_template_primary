874176f49826b04fb19a2282c781648f5aeaccb6
fsj2009yx <289137665@qq.com>
2024-11-17 16:15:41 +0800

    add binarysearch

--- 基础算法/二分查找模板.cpp
+++ 基础算法/二分查找模板.cpp
@@ -1,6 +1,6 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int binarySearch(vector<int>& arr, int left, int right, int target) 
+int binarySearch(vector<int> &arr, int left, int right, int target)
 {
     int mid;
     while (left < right)
@@ -8,16 +8,46 @@ int binarySearch(vector<int>& arr, int left, int right, int target)
         mid = left + (right - left) / 2;
         if (arr[mid] == target)
         {
-            return mid;// 找到
+            return mid; // 找到
         }
-        else if (arr[mid] < target)// 说明在右边
+        else if (arr[mid] < target) // 说明在右边
         {
             left = mid + 1;
         }
-        else// 说明在左边
+        else // 说明在左边
         {
             right = mid;
         }
     }
-    return -1;// 未找到
+    return -1; // 未找到
+}
+// 左侧二分查找
+// 在target值在数组中有多个时，返回最左侧的target值的下标
+int leftBinarySearch(vector<int> &arr, int left, int right, int target)
+{
+    int mid;
+    while (left < right)
+    {
+        mid = left + (right - left) / 2;
+        if (arr[mid] == target)
+        {
+            if (mid > left && arr[mid - 1] == target)
+            {
+                right = mid;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        else if (arr[mid] < target) // 说明在右边
+        {
+            left = mid + 1;
+        }
+        else // 说明在左边
+        {
+            right = mid;
+        }
+    }
+    return -1; // 未找到
 }
